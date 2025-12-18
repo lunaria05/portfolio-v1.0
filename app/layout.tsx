de754614deb771be6dfcc01b90304586dae2668f
Hiralvala563 <hiralvala@lampros.tech>
2025-12-18 18:00:28 +0530

    Added GTM script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,21 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Space_Grotesk } from 'next/font/google'
-import { Poppins } from 'next/font/google'
+import { Space_Grotesk } from "next/font/google";
+import { Poppins } from "next/font/google";
+import Script from "next/script";
 
 const poppins = Poppins({
-  subsets: ['latin'],
+  subsets: ["latin"],
   variable: "--font-poppins",
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-})
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
   variable: "--font-space-grotesk",
-})
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "3D Developer Portfolio | Hand Tracking",
-  description: "An immersive 3D portfolio experience with hand gesture controls powered by MediaPipe",
+  description:
+    "An immersive 3D portfolio experience with hand gesture controls powered by MediaPipe",
 };
 
 export default function RootLayout({
@@ -38,9 +40,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TJS8G5BZ');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${spaceGrotesk.className} ${poppins.className} antialiased`}
       >
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJS8G5BZ"
+height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
         {children}
       </body>
     </html>
